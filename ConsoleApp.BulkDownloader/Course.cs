811d2aebde7ae87f8eac53a93974323b614f97da
asifashraf <asif.log@gmail.com>
2016-09-30 00:23:39 +0500

    Added console to the folder

---
+++ ConsoleApp.BulkDownloader/Course.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.BulkDownloader
+{
+    public class Course
+    {
+        public string Title { get; set; }
+
+        public string Level { get; set; }
+
+        public string Duration { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        public string Name { get; set; }
+
+        public bool HasTranscript { get; set; }
+
+        public bool IsRetired { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsBookmarked { get; set; }
+
+        public bool UserMaySaveCourse { get; set; }
+
+        public string ReplacementCourseName { get; set; }
+
+        public string RetiredReason { get; set; }
+
+        public string ReplacementCourseTitle { get; set; }
+
+        public bool IsValid { get; set; }
+
+        public bool IsUserAuthorizedForTranscript { get; set; }
+    }
+}
---
+++ ConsoleApp.BulkDownloader/Program.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.BulkDownloader
+{
+    class Program
+    {
+        static List<string> items = new List<string>();
+        static System.Timers.Timer timer = new System.Timers.Timer();
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Started");
+
+            var plural = getUrls("plural.txt");
+
+            for (int i = 0; i < plural.Count; i++)
+            {
+                plural[i] = plural[i].Replace(
+                    "https://app.pluralsight.com/library/courses/", String.Empty);
+            }
+
+            items = plural.ToList();
+
+            Console.WriteLine("Parsed file");
+
+            timer.Interval = 5000;
+
+            timer.Elapsed += timer_Elapsed;
+
+            timer.Start();
+
+            Console.ReadKey();
+        }
+
+        static void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine("timer elapsed: {0}", DateTime.Now.ToLongTimeString());
+
+            timer.Stop();
+
+            if (items.Count > 0)
+            {
+                Course course;
+                var name = items[0];
+                Console.WriteLine("Processing course: {0}", name);
+                using (var webClient = new WebClient())
+                {
+                    try
+                    {
+                        var json = webClient.DownloadString(string.Format(settings.COURSE_DATA_URL, name));
+                        course = JsonConvert.DeserializeObject<Course>(json);
+
+                        var url = String.Format("{0}/Home/Index?course={1}", settings.plural, name);
+
+                        Console.WriteLine("Course duration found: {0}; starting process", course.Duration);
+
+                        Process.Start(url);
+
+                        items = items.Where(item => item.ToLower() != name.ToLower()).ToList();
+                        if (items.Count > 0)
+                        {
+                            var segments = course.Duration.Split(new char[] { ':' });
+                            var hours = int.Parse(segments[0]);
+                            var minutes = int.Parse(segments[1]);
+                            var seconds = int.Parse(segments[2]);
+
+                            var span = new TimeSpan(hours, minutes, seconds);
+
+                            var nextInterval = Convert.ToDouble(Math.Floor(
+                                Convert.ToDecimal(span.TotalMilliseconds) / settings.division_factor));
+
+                            timer.Interval = nextInterval;
+
+                            timer.Start();
+
+                            var nextSpan = new TimeSpan(0, 0, 0, 0, Convert.ToInt32( timer.Interval ));
+
+                            Console.WriteLine("Next process will be: {0}; in: {1} time", items[0], nextSpan.ToString());
+                        }
+                    }
+                    catch (Exception error)
+                    {
+                        Console.WriteLine("Error in connecting to pluralsite, We will retry in 1 minute");
+
+                        timer.Interval = 60000;
+
+                        timer.Elapsed += timer_Elapsed;
+
+                        timer.Start();
+
+                    }
+                }
+            }
+        }
+
+        static List<String> getUrls(string fileName)
+        {
+            var dir = String.Format("{0}\\{1}", Environment.CurrentDirectory, fileName);
+            var lines = File.ReadAllLines(dir);
+            List<string> http = new List<string>();
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i].ToLower().Trim();
+                if (line.StartsWith("http") && line.Contains("://"))
+                {
+                    http.Add(lines[i].Trim());
+                }
+            }
+            return http;
+        }
+    }
+}
